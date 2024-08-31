45e7ef96722440f8653ea328b69511d24a01119e
kiki-kanri <a470666@gmail.com>
2024-08-31 14:54:12 +0800

    feat: add login check with redirect and load tabs functionality

---
+++ plugins/11.auth.ts
@@ -0,0 +1,7 @@
+export default defineNuxtPlugin(async () => {
+	const urlPath = useRoute().fullPath;
+	const isLoginPage = urlPath.startsWith('/login');
+	if ((await updateAdminInfoState()).id) {
+		if (isLoginPage) assignToUrlOrNavigateTo(flattenToSingleValue(useRoute().query.redirect, '/'));
+	} else if (!isLoginPage) assignToUrlOrNavigateTo('/login', true);
+});
