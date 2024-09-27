a4511430558a0f1f1cb9cf342846c4206ffb4357
kiki-kanri <a470666@gmail.com>
2024-09-28 00:23:04 +0800

    fix: correct axios interceptor handling of api errors

--- plugins/10.api.client.ts
+++ plugins/10.api.client.ts
@@ -5,14 +5,7 @@ export default defineNuxtPlugin(() => {
 			if (!(error instanceof AxiosError)) throw error;
 			if (!error.response) return ElNotification.error('請檢查網路連線！'), undefined;
 			if (error.response.status === 401) return assignToUrlOrNavigateTo('/login', true), undefined;
-			error.response.error = error.response.data;
-			error.response.data = { message: '系統錯誤！', success: false };
-			try {
-				const errorMessageData = JSON.parse(error.response.error?.message || '{}');
-				error.response.data.data = errorMessageData.data;
-				error.response.data.message = errorMessageData.message || '系統錯誤！';
-				if (accountState.autoUpdateTwoFactorAuthenticationStatus && error.response.data.data?.requiredTwoFactorAuthentications) Object.assign(accountState.twoFactorAuthenticationStatus, error.response.data.data.requiredTwoFactorAuthentications);
-			} catch {}
+			if (accountState.autoUpdateTwoFactorAuthenticationStatus && error.response.data.data?.requiredTwoFactorAuthentications) Object.assign(accountState.twoFactorAuthenticationStatus, error.response.data.data.requiredTwoFactorAuthentications);
 			ElNotification.error(error.response.data.message);
 			return error.response;
 		}
