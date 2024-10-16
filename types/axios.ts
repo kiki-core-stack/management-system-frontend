023f11104d308b47aa5e52c4c75cbc025f6b3a74
kiki-kanri <a470666@gmail.com>
2024-10-16 16:07:45 +0800

    chore: remove unused types files

--- types/axios.ts
+++
@@ -1,7 +0,0 @@
-import type { H3Error } from 'h3';
-
-declare module 'axios' {
-	interface AxiosResponse<T = any, D = any> {
-		error?: H3Error;
-	}
-}
--- types/vue.ts
+++
@@ -1,6 +0,0 @@
-import type { ComponentCustomOptions as _ComponentCustomOptions, ComponentCustomProperties as _ComponentCustomProperties } from 'vue';
-
-declare module '@vue/runtime-core' {
-	interface ComponentCustomOptions extends _ComponentCustomOptions {}
-	interface ComponentCustomProperties extends _ComponentCustomProperties {}
-}
