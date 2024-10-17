d2f533a5e7ce75f1ef28596a091eeb6b90d9ca2b
kiki-kanri <a470666@gmail.com>
2024-10-17 14:25:20 +0800

    feat: add handling for status code 502 in axios API response interceptor

--- plugins/10.api.client.ts
+++ plugins/10.api.client.ts
@@ -5,6 +5,7 @@ export default defineNuxtPlugin(() => {
 			if (!(error instanceof AxiosError)) throw error;
 			if (!error.response) return ElNotification.error('請檢查網路連線！'), undefined;
 			if (error.response.status === 401) return assignToUrlOrNavigateTo('/login', true), undefined;
+			if (error.response.status === 502) return ElNotification.error('系統錯誤！'), undefined;
 			if (accountState.autoUpdateTwoFactorAuthenticationStatus && error.response.data.data?.requiredTwoFactorAuthentications) Object.assign(accountState.twoFactorAuthenticationStatus, error.response.data.data.requiredTwoFactorAuthentications);
 			ElNotification.error(error.response.data.message);
 			return error.response;
