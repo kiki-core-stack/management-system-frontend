207b3649669482108ef946e5feacba82a1d58ef4
kiki-kanri <a470666@gmail.com>
2024-11-22 10:48:00 +0800

    style: change scripts indentation to 4 spaces

--- tmux-run-dev.sh
+++ tmux-run-dev.sh
@@ -3,6 +3,6 @@
 name='kiki-core-stack-admin-frontend'
 
 if ! tmux ls | grep -q "^$name:"; then
-	tmux new-session -ds "$name"
-	tmux send-keys -t "$name" 'pnpm run dev' C-m
+    tmux new-session -ds "$name"
+    tmux send-keys -t "$name" 'pnpm run dev' C-m
 fi
