4dd005dab7db56c6e11290384d904547052953c0
kiki-kanri <a470666@gmail.com>
2025-01-23 12:25:54 +0800

    fix: correct incorrect `rm` and `cp` command paths in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 dist_dir_path='/admin-frontend-dist'
-rm -rf "$dist_dir_path"/{.,}*
-cp -pr /app/public/{.,}* "$dist_dir_path"/
+find $dist_dir_path -mindepth 1 -maxdepth 1 -exec rm -rf {} +
+find /app/public -mindepth 1 -maxdepth 1 -exec cp -pr {} "$dist_dir_path"/ +
 find $dist_dir_path -type d -exec chmod 755 {} +
 find $dist_dir_path -type f -exec chmod 644 {} +
