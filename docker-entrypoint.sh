ebadce204d6cf674d21ce34a055446cf3a0b66d0
kiki-kanri <a470666@gmail.com>
2025-01-23 12:34:15 +0800

    fix: correct incorrect find and cp command in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 dist_dir_path='/admin-frontend-dist'
 find $dist_dir_path -mindepth 1 -maxdepth 1 -exec rm -rf {} +
-find /app/public -mindepth 1 -maxdepth 1 -exec cp -pr {} "$dist_dir_path"/ +
+find /app/public -mindepth 1 -maxdepth 1 -exec cp -pr {} "$dist_dir_path"/ \;
 find $dist_dir_path -type d -exec chmod 755 {} +
 find $dist_dir_path -type f -exec chmod 644 {} +
