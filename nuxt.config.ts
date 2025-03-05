d26912d55a042af22dd4488c7031f14d45d9cd8b
kiki-kanri <a470666@gmail.com>
2025-03-05 10:41:48 +0800

    feat: add `postcss-pxtorem` postcss plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,7 @@ export default defineNuxtConfig({
         },
     },
     modules: ['@kikiutils/nuxt'],
+    postcss: { plugins: { 'postcss-pxtorem': {} } },
     ssr: true,
     vite: {
         server: {
