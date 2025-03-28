014bab27863a0a4d1d778c4cae5f8718f23425ee
kiki-kanri <a470666@gmail.com>
2025-03-29 05:15:04 +0800

    chore: modify `docker-entrypoint` to set target path for static files to `/static` to simplify future naming issues

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 set -e
-dist_dir_path='/admin-frontend-dist'
-rm -rf "$dist_dir_path"/*
-cp -r /app/public/* "$dist_dir_path"/
-find $dist_dir_path -type d -exec chmod 755 {} +
-find $dist_dir_path -type f -exec chmod 644 {} +
+static_dir_path='/static'
+rm -rf "$static_dir_path"/*
+cp -r /app/public/* "$static_dir_path"/
+find $static_dir_path -type d -exec chmod 755 {} +
+find $static_dir_path -type f -exec chmod 644 {} +
