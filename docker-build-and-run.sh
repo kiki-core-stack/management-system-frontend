1544adf3f4520a46f289d23df7f1ecb5af5ebb04
kiki-kanri <a470666@gmail.com>
2025-03-31 02:35:37 +0800

    refactor: update Docker-related scripts
    - simplify scripts
    - add comments and appropriate spacing

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
 set -e
+
+# Load and set variables
 . ./.env.production.local
-container_name='nuxt-project'
-bind_port='127.0.0.1:3000'
-image_tag='user/nuxt-project:latest'
+author='user'
+base_name='nuxt-project'
+container_name="$base_name"
+bind_address='127.0.0.1:3000'
+image_tag="$author/$base_name:latest"
 static_dir_bind_path='/data/web/nuxt-project/static'
 
+# Pull images
 docker pull kikikanri/node:22-slim &
 docker pull node:22-slim &
 wait
+
+# Build and run
 docker build \
     -t "$image_tag" \
     --build-arg "NPM_REGISTRY=$NPM_REGISTRY" \
     .
 
-docker kill "$container_name" || true
+docker stop "$container_name" || true
 docker rm "$container_name" || true
 docker run \
     -itd \
-    -p "$bind_port:3000" \
+    -p "$bind_address:3000" \
     -v "$static_dir_bind_path:/static" \
     --name "$container_name" \
     --restart=always \
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
 set -e
+
+# Copy static files and set permissions
 static_dir_path='/static'
 rm -rf "$static_dir_path"/*
 cp -r /app/public/* "$static_dir_path"/
-find $static_dir_path -type d -exec chmod 755 {} +
-find $static_dir_path -type f -exec chmod 644 {} +
+find "$static_dir_path" -type d -exec chmod 755 {} +
+find "$static_dir_path" -type f -exec chmod 644 {} +
+
+# Load secrets to environment
+if [ -d /run/secrets ]; then
+    for secret_file_path in /run/secrets/*; do
+        [ ! -f "$secret_file_path" ] && continue
+        secret_key=$(basename "$secret_file_path")
+        secret_value=$(cat "$secret_file_path")
+        export "${secret_key}"="${secret_value}"
+    done
+fi
+
+# Start server
 cd /app/server
 exec node ./index.mjs
