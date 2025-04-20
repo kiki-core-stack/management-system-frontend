230492af7126fff2e29690c1f4f923130b1dfc96
kiki-kanri <a470666@gmail.com>
2025-04-20 21:37:50 +0800

    chore: update all scripts

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,4 +2,8 @@
 
 . ./.env.development.local
 export NPM_CONFIG_REGISTRY
+
+set -e
+
 pnpm i
+./modify-files-permissions.sh
--- upgrade-dependencies.sh
+++ upgrade-dependencies.sh
@@ -2,4 +2,8 @@
 
 . ./.env.development.local
 export NPM_CONFIG_REGISTRY
+
+set -e
+
 pnpm upgrade -L
+./modify-files-permissions.sh
