47b90b3313a32526c29277b9907e496c3fda7cf3
kiki-kanri <a470666@gmail.com>
2025-04-28 17:32:34 +0800

    feat: add scope bind to `DataTablePage`'s `action-btns-prepend` and `action-btns-append` slots

--- components/DataTablePage.vue
+++ components/DataTablePage.vue
@@ -66,7 +66,10 @@
                     >
                         <template #default="scope">
                             <div class="flex-middle gap-btns">
-                                <slot name="action-btns-prepend" />
+                                <slot
+                                    :="scope"
+                                    name="action-btns-prepend"
+                                />
                                 <el-action-btn
                                     v-if="!hideEditBtn && !hideRowEditBtnRule?.(scope.row)"
                                     :disabled="disableRowEditBtnRule?.(scope.row)"
@@ -82,7 +85,10 @@
                                 >
                                     刪除
                                 </el-action-btn>
-                                <slot name="action-btns-append" />
+                                <slot
+                                    :="scope"
+                                    name="action-btns-append"
+                                />
                             </div>
                         </template>
                     </el-table-column>
