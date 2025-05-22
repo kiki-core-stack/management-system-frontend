ff700e9345ebe6583db066771f78cff1866e97d3
kiki-kanri <a470666@gmail.com>
2025-05-22 09:40:48 +0800

    feat: enhance `ElTableDatetimeColumn` component
    - add `emptyText` prop for fallback display when value is missing
    - use `lodash-es` `get` to support nested field access like `xxx.xxx`

--- components/El/Table/DatetimeColumn.vue
+++ components/El/Table/DatetimeColumn.vue
@@ -2,15 +2,31 @@
     <el-table-column
         align="center"
         width="156"
-        :formatter="(row) => formatDate(row[field])"
+        :formatter="formatDateCell"
     />
 </template>
 
 <script lang="ts" setup>
+import { get } from 'lodash-es';
+
 interface Props {
+    emptyText?: string;
     field?: string;
 }
 
 // Define props, models and emits
-withDefaults(defineProps<Props>(), { field: 'createdAt' });
+const props = withDefaults(
+    defineProps<Props>(),
+    {
+        emptyText: '',
+        field: 'createdAt',
+    },
+);
+
+// Functions
+function formatDateCell(row: any) {
+    const value = get(row, props.field);
+    if (value === undefined) return props.emptyText;
+    return formatDate(value);
+}
 </script>
