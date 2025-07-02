cab440aebb445b0e788374bddfba895f74a9f580
kiki-kanri <a470666@gmail.com>
2025-07-02 19:11:53 +0800

    refactor: update `BaseCrudApi.create` response type to return created data from API

--- app/libs/apis/_internals/base/crud.ts
+++ app/libs/apis/_internals/base/crud.ts
@@ -5,7 +5,7 @@ import { BaseApi } from './';
 export abstract class BaseCrudApi<T extends TableRowData = TableRowData> extends BaseApi {
     async create(data: OmitMongooseTimestampAndOtherFields<T>, config?: AxiosRequestConfig) {
         try {
-            return await this.postRequest(undefined, await this.processCreateOrUpdateData(data), config);
+            return await this.postRequest<T>(undefined, await this.processCreateOrUpdateData(data), config);
         } catch {}
     }
 
