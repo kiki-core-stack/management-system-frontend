54d96a5fda7bdd293a0a26c22e7ec90f161ce310
kiki-kanri <a470666@gmail.com>
2025-07-02 15:01:47 +0800

    feat: use View Transitions API to handle light/dark mode transition animations

--- app/components/Layout/Header/index.vue
+++ app/components/Layout/Header/index.vue
@@ -9,15 +9,15 @@
         />
         <span class="fs-20px">後台管理系統</span>
         <div class="grow" />
-        <i-fa6-solid-sun class="mr-2" />
-        <el-switch
-            v-model="colorMode.preference"
-            active-value="dark"
-            inactive-value="light"
-        />
-        <i-fa6-solid-moon class="ml-2" />
         <el-button
-            class="ml-2"
+            class="rounded-full! w-1"
+            @click="toggleTheme"
+        >
+            <i-fa6-solid-moon v-if="isDark" />
+            <i-fa6-solid-sun v-else />
+        </el-button>
+        <el-button
+            class="ml-1!"
             @click="logout()"
         >
             登出
@@ -31,6 +31,39 @@ import { logout } from '@/libs/auth';
 // Variables
 const colorMode = useColorMode();
 const sidebarState = useSidebarState();
+
+// Computed properties
+const isDark = computed(() => colorMode.preference === 'dark');
+
+// Functions
+const toggleDark = () => void (colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark');
+
+function toggleTheme(event: MouseEvent) {
+    const button = event.currentTarget as HTMLElement;
+    const rect = button.getBoundingClientRect();
+    const x = rect.left + rect.width / 2;
+    const y = rect.top + rect.height / 2;
+    const endRadius = Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y));
+    if (!document.startViewTransition) return void toggleDark();
+    const transition = document.startViewTransition(() => toggleDark());
+    transition.ready.then(() => {
+        const clipPath = [
+            `circle(0px at ${x}px ${y}px)`,
+            `circle(${endRadius}px at ${x}px ${y}px)`,
+        ];
+
+        document.documentElement.animate(
+            { clipPath: isDark.value ? [...clipPath].reverse() : clipPath },
+            {
+                duration: 400,
+                easing: 'ease-in-out',
+                pseudoElement: isDark.value
+                    ? '::view-transition-old(root)'
+                    : '::view-transition-new(root)',
+            },
+        );
+    });
+}
 </script>
 
 <style lang="scss" scoped>
