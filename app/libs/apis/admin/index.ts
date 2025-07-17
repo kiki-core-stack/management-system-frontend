09e911de69edd3044d4ca161b069c01590c4e1df
kiki-kanri <a470666@gmail.com>
2025-07-17 17:00:39 +0800

    refactor: update `AdminApi.processCreateOrUpdateData`

--- app/libs/apis/admin/index.ts
+++ app/libs/apis/admin/index.ts
@@ -1,18 +1,22 @@
 import type { AdminData } from '@kiki-core-stack/pack/types/data/admin';
 
+import type { AdminFormData } from '@/types/data/admin';
+
 import { BaseCrudApi } from '../_internals/base/crud';
 
-export class AdminApi extends BaseCrudApi<AdminData> {
+export class AdminApi extends BaseCrudApi<AdminData, AdminFormData> {
     constructor() {
         super('/api/admin');
     }
 
-    // eslint-disable-next-line require-await
-    override async processCreateOrUpdateData(data: AdminData) {
-        data = cloneDeep(data);
-        if (!data.email?.trim()) delete data.email;
-        if (data.password) data.password = sha3512(data.password);
-        else delete data.password;
-        return data;
+    override processCreateOrUpdateData(data: AdminFormData) {
+        const clonedData: Partial<AdminFormData> = cloneDeep(data);
+
+        if (!clonedData.email?.trim()) delete clonedData.email;
+
+        if (clonedData.password) clonedData.password = sha3512(data.password);
+        else delete clonedData.password;
+
+        return clonedData;
     }
 }
