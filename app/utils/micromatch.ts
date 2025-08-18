d051048b9b126374baa0737b6b08f9d71006cc7f
kiki-kanri <a470666@gmail.com>
2025-08-18 15:11:08 +0800

    chore: replace `micromatch` with browser-compatible version

--- app/utils/micromatch.ts
+++ app/utils/micromatch.ts
@@ -1,5 +1,5 @@
 // @ts-expect-error Ignore this error.
-import _micromatch from 'micromatch';
+import _micromatch from '@kikiutils/micromatch';
 import type * as micromatchType from 'micromatch';
 
 export const micromatch = _micromatch as typeof micromatchType;
