c9a5e902e698425d20fd85d960725c1b699bd87d
kiki-kanri <a470666@gmail.com>
2025-08-19 14:56:39 +0800

    feat(permission): update `hasPermission` param type to `...requiredPermissions: Arrayable<AdminPermissionPattern>[]`

--- app/globals/libs/admin.ts
+++ app/globals/libs/admin.ts
@@ -1,14 +1,13 @@
 import type { AdminPermissionPattern } from '@/types/admin';
 
-export function hasPermission(requiredPermissions: Arrayable<AdminPermissionPattern>) {
-    if (
-        requiredPermissions === 'ignore'
-        || (Array.isArray(requiredPermissions) && requiredPermissions.includes('ignore'))
-    ) return true;
+export function hasPermission(...requiredPermissions: Arrayable<AdminPermissionPattern>[]) {
+    const permissions = requiredPermissions.flat();
+
+    if (permissions.includes('ignore')) return true;
 
     const profileState = useProfileState();
     return (
         profileState.value.permission.isSuperAdmin
-        || micromatch(profileState.value.permission.permissions, requiredPermissions).length > 0
+        || micromatch(profileState.value.permission.permissions, permissions).length > 0
     );
 }
