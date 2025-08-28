aa2193fb20f5638ba87a15b13d596544cadf0bca
kiki-kanri <a470666@gmail.com>
2025-08-28 10:10:22 +0800

    fix: resolve `customClass` override issue in swal2 alert

--- app/globals/libs/sweetalert2.ts
+++ app/globals/libs/sweetalert2.ts
@@ -12,7 +12,10 @@ export const closeAlert = customSweetAlert.close;
 export function showErrorAlert(title: string, options?: SweetAlertOptions) {
     customSweetAlert.fire({
         ...options,
-        customClass: { confirmButton: 'el-button el-button--danger' },
+        customClass: {
+            confirmButton: 'el-button el-button--danger',
+            ...options?.customClass,
+        },
         icon: 'error',
         title,
     });
@@ -23,7 +26,10 @@ export function showLoadingAlert(title: string = '載入中...', options?: Sweet
         ...options,
         allowEscapeKey: false,
         allowOutsideClick: false,
-        customClass: { actions: 'mt-4' },
+        customClass: {
+            ...options?.customClass,
+            actions: 'mt-4',
+        },
         didOpen: () => customSweetAlert.showLoading(),
         title,
     });
@@ -32,7 +38,10 @@ export function showLoadingAlert(title: string = '載入中...', options?: Sweet
 export function showSuccessAlert(title: string, options?: SweetAlertOptions) {
     customSweetAlert.fire({
         ...options,
-        customClass: { confirmButton: 'el-button el-button--success' },
+        customClass: {
+            ...options?.customClass,
+            confirmButton: 'el-button el-button--success',
+        },
         icon: 'success',
         title,
     });
