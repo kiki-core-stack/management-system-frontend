e88377f5d7ff9c104f8f78481ffa74013af1b76a
kiki-kanri <a470666@gmail.com>
2025-09-18 00:35:22 +0800

    chore: change tmux `SESSION_NAME`

--- tmux-run-dev.sh
+++ tmux-run-dev.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 SCRIPTS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 cd "${SCRIPTS_DIR}"
 
-SESSION_NAME='kiki-core-stack-admin-frontend'
+SESSION_NAME='kiki-core-stack-management-system-frontend'
 
 if ! tmux ls | grep -q "^${SESSION_NAME}:"; then
     tmux new-session -ds "${SESSION_NAME}"
