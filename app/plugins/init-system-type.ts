7d365ca62dfbf2e7c2dc339d65a0be7f5c325373
kiki-kanri <a470666@gmail.com>
2025-09-24 13:22:08 +0800

    refactor: use `getManagementSystemTypeFromRoutePath` to get system type in `init-system-type`

--- app/plugins/init-system-type.ts
+++ app/plugins/init-system-type.ts
@@ -1,9 +1,7 @@
-import type { ManagementSystemType } from '@kiki-core-stack/pack/types';
+import { getManagementSystemTypeFromRoutePath } from '@kiki-core-stack/pack/libs/management-system';
 
 export default defineNuxtPlugin(() => {
-    let systemType: ManagementSystemType | undefined;
-    const urlPath = useRoute().fullPath;
-    if (urlPath.startsWith('/admin')) systemType = 'admin';
-    else throw createError({ statusCode: 404 });
+    const systemType = getManagementSystemTypeFromRoutePath(useRoute().fullPath);
+    if (!systemType) throw createError({ statusCode: 404 });
     return { provide: { systemType } };
 });
